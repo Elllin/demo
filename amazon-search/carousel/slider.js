d4ee2b5dff755243604c9a2713fb9ab503098e28
eunbi <noto0131@gmail.com>
2020-03-06 13:03:23 +0900

    데모

---
+++ amazon-search/carousel/slider.js
@@ -0,0 +1,65 @@
+import { _$ } from "../util.js";
+
+export class CarouselSlider {
+  constructor({ transitionProperty, selector }) {
+    this.slideIndex = 1;
+    this.selector = selector;
+    this.slides = _$(this.selector.slides);
+    this.slideSize = this.slides.firstElementChild.clientWidth;
+    this.transitionProperty = transitionProperty;
+  }
+
+  getSliderInfo() {
+    this.slideItems = _$(this.selector.slideItem, true);
+    this.slideLength = this.slideItems.length;
+    this.lastSlideIndex = this.slideItems.length - 1;
+  }
+
+  cloneSlide() {
+    const { firstElementChild, lastElementChild } = this.slides,
+      firstClone = firstElementChild.cloneNode(true),
+      lastClone = lastElementChild.cloneNode(true);
+
+    firstClone.id = this.selector.firstClone;
+    lastClone.id = this.selector.lastClone;
+    this.slides.append(firstClone);
+    this.slides.prepend(lastClone);
+    this.getSliderInfo();
+    this.moveSlides(this.slideIndex);
+  }
+
+  moveSlides() {
+    return (this.slides.style.transform =
+      "translateX(" + -this.slideSize * this.slideIndex + "px)");
+  }
+
+  addTransition() {
+    const { name, duration, timingFunc } = this.transitionProperty;
+
+    this.slides.style.transition = `${name} ${duration} ${timingFunc}`;
+    this.moveSlides();
+    this.checkCurrentSlideId();
+  }
+
+  checkCurrentSlideId() {
+    this.slides.addEventListener("transitionend", () => {
+      const FIRST_SLIDE_INDEX = 1;
+      const currentSlideId = this.slideItems[this.slideIndex].id;
+
+      let lastSlideIndex = 2;
+      lastSlideIndex = this.slideLength - lastSlideIndex;
+
+      if (currentSlideId === this.selector.lastClone)
+        this.removeTransition(lastSlideIndex);
+
+      if (currentSlideId === this.selector.firstClone)
+        this.removeTransition(FIRST_SLIDE_INDEX);
+    });
+  }
+
+  removeTransition(index) {
+    this.slides.style.transition = "none";
+    this.slideIndex = index;
+    this.moveSlides();
+  }
+}
---
+++ amazon-search/carousel/sliderBtn.js
@@ -0,0 +1,30 @@
+import { _$ } from "../util.js";
+
+export class CarouselSliderBtn {
+  constructor({ carouselSlider, carouselCardMenu, selector }) {
+    this.slider = carouselSlider;
+    this.cardMenu = carouselCardMenu;
+    this.selector = selector;
+  }
+
+  setSliderBtns() {
+    const [previousBtn, nextBtn] = _$(this.selector.sliderBtns, true);
+
+    previousBtn.addEventListener("click", this.setPreviousBtn.bind(this));
+    nextBtn.addEventListener("click", this.setNextBtn.bind(this));
+  }
+
+  setPreviousBtn() {
+    if (this.slider.slideIndex <= 0) return;
+    this.slider.slideIndex--;
+    this.slider.addTransition();
+    this.cardMenu.addScaleEffect(true);
+  }
+
+  setNextBtn() {
+    if (this.slider.slideIndex >= this.slider.lastSlideIndex) return;
+    this.slider.slideIndex++;
+    this.slider.addTransition();
+    this.cardMenu.addScaleEffect(true);
+  }
+}
